bcef8eb867cd615b7befdb28902f513f776c87fc
alexandrucurcubat <alexandrucurcubat@gmail.com>
2021-02-12 11:33:52 +0200

    fix tslint errors

--- src/app/auctions/pipes/seconds-to-dhms.pipe.ts
+++ src/app/auctions/pipes/seconds-to-dhms.pipe.ts
@@ -7,10 +7,10 @@ export class SecondsToDhmsPipe implements PipeTransform {
     const h = Math.floor((value % (3600 * 24)) / 3600);
     const m = Math.floor((value % 3600) / 60);
     const s = Math.floor(value % 60);
-    const dDisplay = d > 0 ? d + (d == 1 ? 'z ' : 'z ') : '';
-    const hDisplay = h > 0 ? h + (h == 1 ? 'h ' : 'h ') : '';
-    const mDisplay = m > 0 ? m + (m == 1 ? 'm ' : 'm ') : '';
-    const sDisplay = s > 0 ? s + (s == 1 ? 's' : 's') : '';
+    const dDisplay = d > 0 ? d + (d === 1 ? 'z ' : 'z ') : '';
+    const hDisplay = h > 0 ? h + (h === 1 ? 'h ' : 'h ') : '';
+    const mDisplay = m > 0 ? m + (m === 1 ? 'm ' : 'm ') : '';
+    const sDisplay = s > 0 ? s + (s === 1 ? 's' : 's') : '';
     return d > 0 ? dDisplay : dDisplay + hDisplay + mDisplay + sDisplay;
   }
 }
